3fe6801416b9dc57a916ab489f3e4d12c757c422
Morgana Morandi <65549682+morganamorandi@users.noreply.github.com>
2025-09-12 00:07:21 +0200

    navbar, keyboard, up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
+//navbar
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleSidebar = document.querySelector('.nav-toggle-btn');
+const navbar = document.querySelector('.navbar');
+
+toggleSidebar.onclick = function() {
+    navbar.classList.toggle('active');
+}
+});
+
+
 // Funzione elemento date
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -168,3 +179,78 @@ if (document.readyState === 'loading') {
     // DOM is already ready
     initCounters();
 }
+
+
+// ------KEYBOARD-------
+
+document.addEventListener('DOMContentLoaded', function() {
+    var $keyboardWrapper = $('.virtual-keyboard'),
+    $key = $keyboardWrapper.find("input"),
+    $key_delete = $('.delete'),
+    $key_shift = $('.shift'),
+    $outputField = $('.output input'),
+    $currentValue = $outputField.val(),
+    actionKeys = $(".delete,.shift")
+    activeShiftClass = "shift-activated";
+
+    
+    window.onload = function() {
+        document.getElementById('.output input').reset();
+    }
+
+    // handle keystrokes
+    function _keystroke(keyCase){
+    
+    $key.not(actionKeys).on('click',function(e){
+        e.preventDefault();
+        
+        // check for shift key for upper
+        if($key_shift.hasClass(activeShiftClass)){
+        keyCase = 'upper';
+        $key_shift.removeClass(activeShiftClass);
+        }else{
+        keyCase = 'lower';
+        }
+        
+        // handle case
+        if(keyCase == 'upper'){
+        var keyValue = $(this).val().toUpperCase();
+        }else{
+        var keyValue = $(this).val().toLowerCase();
+        }
+        
+        // grab current value
+        var output = $('.output input').val();
+            $outputField.val(output + keyValue);
+            getCurrentVal();
+            focusOutputField();
+    });
+    
+    } // keystroke
+    
+    // delete
+    $key_delete.on('click',function(e){
+    e.preventDefault();
+    $outputField.val($currentValue.substr(0,$currentValue.length - 1));
+    getCurrentVal();
+    focusOutputField();
+    });
+
+    // shift
+    $key_shift.on('click',function(e){
+    e.preventDefault();
+    $(this).toggleClass(activeShiftClass);
+    });
+
+    // grab current value of typed text
+    function getCurrentVal(){
+    $currentValue = $outputField.val();
+    }
+
+    // focus for cursor hack
+    function focusOutputField(){
+    $outputField.focus();
+    }
+
+    _keystroke("lower"); // init keystrokes
+});
